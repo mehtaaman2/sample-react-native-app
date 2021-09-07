c31c439c48e3ca823fb2a1de2a9ba8326f82d38f
Aman Mehta <mehta.aman2@gmail.com>
2021-09-08 01:15:57 +0530

    Added loading indicator

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { StyleSheet, Text, View, Button, ToastAndroid, Modal, Pressable, ScrollView, Image } from 'react-native';
+import { StyleSheet, Text, View, Button, ToastAndroid, Modal, Pressable, ScrollView, Image, ActivityIndicator } from 'react-native';
 import InputCard from './components/InputCard';
 import Task from './model/Task';
 import TaskView from './components/Task';
@@ -18,9 +18,11 @@ export default function App() {
   const [showModal, setShowModal] = useState(false);
   const [tasks, setTasks] = useState<Task[]>([]);
   const [currentTask, setCurrentTask] = useState<Task>(new Task());
+  const [loading, setLoading] = useState(true);
 
   async function fetchTasks() {
     console.log("Fetching info!!");
+    setLoading(true);
     let userId = await DeviceInfo.getAndroidId().then((androidId) => {
       return androidId;
     });
@@ -28,6 +30,7 @@ export default function App() {
     let tasks: Task[] = response.data.map((task) => new Task(task));
     console.log('Response from GET API : ', tasks);
     setTasks(tasks);
+    setLoading(false);
   }
 
   useEffect(() => {
@@ -71,7 +74,13 @@ export default function App() {
         </Pressable>
       </View>
       <ScrollView style={styles.scrollView} persistentScrollbar={true}>
-          {tasks.map(function(task, i) {
+          {loading &&
+            <View style={styles.loadingView}>
+              <ActivityIndicator size="large" color="#0000ff"/>
+              <Text>Loading tasks for the user!</Text>
+            </View>
+           }
+          {!loading && tasks.map(function(task, i) {
             return (<View key = {i} style={styles.taskView}>
                       <TaskView task={task}/>
                   </View>)
@@ -80,7 +89,6 @@ export default function App() {
       <View style={styles.addAlertButton}>
         <Button title="Add alert" onPress={() => {setShowModal(true);}}/>
       </View>
-
 
       <Modal visible={showModal}
              transparent={false}
@@ -143,5 +151,9 @@ const styles = StyleSheet.create({
   },
   taskView: {
     padding: 5,
+  },
+  loadingView: {
+    paddingTop: 200,
+    alignItems: 'center',
   }
 });
